54c791410adb32d5c3851f8c228610f6c65ed738
kristiyan.valchev <krisvalchev@gmail.com>
2024-09-06 18:53:27 +0300

    Added trpe for order

--- frontend/src/app/types/product.ts
+++ frontend/src/app/types/product.ts
@@ -1,1 +1,15 @@
-export type Product = {};
+export type Product = {
+   id: string;
+   name: string;
+   shortDescription: string;
+   description: string;
+   categoryId: string;
+   price: number;
+   discount?: number;
+   quantity?: number;
+   markAsNew?: boolean;
+   coverPhoto: string;
+   photos: string[];
+   sizes: string[];
+   colors: string[];
+};
