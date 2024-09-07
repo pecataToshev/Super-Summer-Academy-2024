260d7c9f255d021da2cd44407ef5aba7b0d89b31
Petar Toshev <pecata.toshev@gmail.com>
2024-09-07 14:37:39 +0300

    increase file size

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+function setupSwagger(app) {
   const config = new DocumentBuilder()
     .setTitle('Super Summer Academy 2024')
     .setDescription('The Task done by Petar and Kris')
@@ -12,7 +11,16 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
+
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
   await app.listen(3000);
 }
-bootstrap();
+
+bootstrap().then(() => ({}));
